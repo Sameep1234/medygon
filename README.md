e04128796d53680bf7832a1576ba41e3b1c9c159
malav312 <malavdoshi312@gmailcom>
2022-03-26 22:13:17 +0530

    kuch bhi

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 ## Flow
 Ethereum -> Polygon -> Hardhat -> IPFS -> Graph (Indexing) -> Metamask -> Metamask -> Frontend
 
+## Stakeholders
+- Patient
+- Doctor
+- General Viewer
+
+## Features
+- The medical records are stored securely using the IPFS.
+- Allows patient to revoke access to doctors in case he doesn't want them to access it.
+- Allows Doctors to upload patient records and give access to other doctors.
+- 
+## Smart Contracts
+- 
--- hardhat/contracts/medContract.sol
+++ hardhat/contracts/medContract.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract medygon {
-    mapping(address => doctor) doctorId;
-    mapping(address => patient) patientId;
+    mapping(address => doctor() doctorId;
+    mapping(address => patient() patientId;
 
     struct recordHash {
         address ipfsHash;
@@ -23,19 +23,19 @@ contract medygon {
 
     function hashToPatient(address patientId) private view {}
 
-    function viewDoctor(address _owner)
-        external
-        view
-        returns (uint256[] memory)
-    {
-        uint256[] memory result = new uint256[](ownerZombieCount[pa]);
-        uint256 counter = 0;
-        for (uint256 i = 0; i < zombies.length; i++) {
-            if (zombieToOwner[i] == _owner) {
-                result[counter] = i;
-                counter++;
-            }
-        }
-        return result;
-    }
+    // function viewDoctor(address _owner)
+    //     external
+    //     view
+    //     returns (uint256[] memory)
+    // {
+    //     uint256[] memory result = new uint256[](ownerZombieCount[pa]);
+    //     uint256 counter = 0;
+    //     for (uint256 i = 0; i < zombies.length; i++) {
+    //         if (zombieToOwner[i] == _owner) {
+    //             result[counter] = i;
+    //             counter++;
+    //         }
+    //     }
+    //     return result;
+    // }
 }
